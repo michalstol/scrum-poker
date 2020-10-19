895f25f59c5472a35e1824f991689c03795faeda
Michal <michalstolarski@gmail.com>
2020-10-19 19:47:13 +0200

    remove next :( from repo - firebase hosting is not working easy with nextjs

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps /*, AppContext */ } from 'next/app'
-import '../styles/globals.css'
-
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default App;
