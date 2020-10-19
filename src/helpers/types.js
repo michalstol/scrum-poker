6fa4fdbf3eb251fc734880083b6541397e82609f
Michal <michalstolarski@gmail.com>
2020-10-19 18:46:43 +0200

    create env

--- src/helpers/types.js
+++
@@ -1,9 +0,0 @@
-// suffixes action for redux-promise-middleware
-export const PENDING = '_PENDING';
-export const REJECTED = '_REJECTED';
-export const FULFILLED = '_FULFILLED';
-
-// prefixes action
-export const ADD = 'ADD_';
-export const FETCH = 'FETCH_';
-export const REMOVED = 'REMOVED_';
