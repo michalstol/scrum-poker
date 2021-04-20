499e95e3a6b29a0398ee05c4ac37cfe3e49082db
Michal <michalstolarski@gmail.com>
2021-04-20 18:37:55 +0200

    remove fireUp event

--- src/components/form-components/Button/Button.test.tsx
+++ src/components/form-components/Button/Button.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, fireEvent, render } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 
 import Button from './Button';
 
