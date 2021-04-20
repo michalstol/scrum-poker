7891dda373809082a4f54c5f6fbc3c68a8d1909c
Michal <michalstolarski@gmail.com>
2021-04-20 18:37:35 +0200

    create test for button

---
+++ src/components/form-components/Button/Button.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
+
+import Button from './Button';
+
+afterEach(cleanup);
+
+it('Button component - render a button with the secondary variant', () => {
+    const variant = 'secondary';
+    const children = 'Text';
+
+    const { container } = render(<Button variant={variant}>{children}</Button>);
+    const { firstChild } = container;
+
+    expect(firstChild).toHaveClass(`button--${variant}`);
+    expect(firstChild).toHaveTextContent(children);
+});
---
+++ src/components/form-components/Button/Button.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import classNames from 'classnames';
+
+interface ButtonInterface extends React.HTMLAttributes<HTMLButtonElement> {
+    variant?: 'active' | 'distance' | 'distance-small' | 'secondary' | 'revers';
+    children: React.ReactNode;
+}
+
+/**
+ *
+ * @param { 'active' | 'distance' | 'distance-small' | 'secondary' | 'revers' } version - Avaiable visual versions of the component
+ * @param { React.ReactNode } children - React children
+ * @returns { JSX.Element }
+ */
+export default function Button({
+    variant,
+    children,
+    ...props
+}: ButtonInterface): JSX.Element {
+    return (
+        <button
+            className={classNames('button', {
+                [`button--${variant}`]: !!variant,
+            })}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+}
